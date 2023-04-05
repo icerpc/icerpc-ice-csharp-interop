2ef506d1baf061f38b2d1779573dcbf03dfccc61
Jose <jose@zeroc.com>
2023-04-05 11:05:35 +0200

    Review fixes

--- tests/Interop.Tests/RequestContextTests.cs
+++ tests/Interop.Tests/RequestContextTests.cs
@@ -7,17 +7,18 @@ using IceRpc.RequestContext;
 using IceRpc.Slice;
 using Interop.Tests.Slice;
 using NUnit.Framework;
+using System.Collections.Immutable;
 
 namespace Interop.Tests;
 
 [Parallelizable(scope: ParallelScope.All)]
 public class RequestContextTests
 {
-    private static IEnumerable<Dictionary<string, string>> RequestContextSource
+    private static IEnumerable<IDictionary<string, string>> RequestContextSource
     {
         get
         {
-            yield return new Dictionary<string, string>();
+            yield return ImmutableDictionary<string, string>.Empty;
             yield return new Dictionary<string, string>
             {
                 ["Foo"] = "Bar",
@@ -28,50 +29,45 @@ public class RequestContextTests
 
     /// <summary>Sends a request with a request context from Ice to IceRPC.</summary>
     [TestCaseSource(nameof(RequestContextSource))]
-    public async Task Send_request_with_context_from_Ice_to_IceRpc(
-        Dictionary<string, string> context)
+    public async Task Send_request_with_context_from_Ice_to_IceRpc(IDictionary<string, string> context)
     {
         // Arrange
-        var chatBoot = new ChatBotTwin();
-        await using var server = new Server(
-            new RequestContextMiddleware(chatBoot),
-            new Uri("ice://127.0.0.1:0"));
+        var chatBot = new ChatBotTwin();
+        await using var server = new Server(new RequestContextMiddleware(chatBot), new Uri("ice://127.0.0.1:0"));
         ServerAddress serverAddress = server.Listen();
 
         using Communicator communicator = Util.initialize();
-        GreeterPrx proxy = GreeterPrxHelper.uncheckedCast(communicator.CreateObjectPrx("greater", serverAddress));
+        GreeterPrx proxy = GreeterPrxHelper.uncheckedCast(communicator.CreateObjectPrx("greeter", serverAddress));
 
         // Act
-        await proxy.greetAsync("Bob", context);
+        await proxy.greetAsync("Bob", new Dictionary<string, string>(context));
 
         // Assert
-        Assert.That(async () => await chatBoot.RequestContext, Is.EqualTo(context));
+        Assert.That(async () => await chatBot.RequestContext, Is.EqualTo(context));
     }
 
     /// <summary>Sends a request with a request context from IceRPC to Ice.</summary>
     [TestCaseSource(nameof(RequestContextSource))]
-    public async Task Send_request_with_context_from_IceRpc_to_Ice(
-        Dictionary<string, string> context)
+    public async Task Send_request_with_context_from_IceRpc_to_Ice(IDictionary<string, string> context)
     {
         // Arrange
-        var chatBoot = new ChatBot();
+        var chatBot = new ChatBot();
         using Communicator communicator = Util.initialize();
         ObjectAdapter adapter = communicator.createObjectAdapterWithEndpoints("test", "tcp -h 127.0.0.1 -p 0");
-        _ = adapter.add(chatBoot, Util.stringToIdentity("greater"));
+        _ = adapter.add(chatBot, Util.stringToIdentity("greeter"));
         adapter.activate();
 
         await using var clientConnection = new ClientConnection(adapter.GetFirstServerAddress());
-        Pipeline pipeline = new Pipeline().UseRequestContext().Into(clientConnection);
-        var proxy = new GreeterProxy(pipeline, new Uri("ice:/greater"));
+        var proxy = new GreeterProxy(new RequestContextInterceptor(clientConnection), new Uri("ice:/greeter"));
 
         var features = new FeatureCollection();
-        features.Set<IRequestContextFeature>(new RequestContextFeature{ Value = context });
+        features.Set<IRequestContextFeature>(new RequestContextFeature { Value = context });
 
         // Act
         await proxy.GreetAsync("Bob", features);
 
         // Assert
-        Assert.That(async () => await chatBoot.RequestContext, Is.EqualTo(context));
+        Assert.That(async () => await chatBot.RequestContext, Is.EqualTo(context));
     }
 
     private class ChatBot : GreeterDisp_
