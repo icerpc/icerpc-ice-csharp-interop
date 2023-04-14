a1c654c1b1f5bd55204aa8e64a386b849aca8f22
Benoit Foucher <benoit@zeroc.com>
2023-04-14 15:54:45 +0200

    Added enumeration test (#41)

---
+++ tests/Interop.Tests/Slice/EnumTests.cs
@@ -0,0 +1,67 @@
+// Copyright (c) ZeroC, Inc.
+
+using Ice;
+using IceRpc.Slice;
+using NUnit.Framework;
+using System.Buffers;
+using System.IO.Pipelines;
+
+namespace Interop.Tests.Slice;
+
+[Parallelizable(scope: ParallelScope.All)]
+public class EnumTests
+{
+    [Test]
+    public void Ice_my_enum_to_slice_my_enum()
+    {
+        MyEnumTwin decodedValue = IceToSlice(
+            MyEnum.Enum1,
+            MyEnumHelper.write,
+            MyEnumTwinSliceDecoderExtensions.DecodeMyEnumTwin);
+
+        Assert.That(decodedValue, Is.EqualTo(MyEnumTwin.Enum1));
+    }
+
+    [Test]
+    public void Slice_my_enum_to_ice_my_enum()
+    {
+        MyEnum decodedValue = SliceToIce(
+            MyEnumTwin.Enum1,
+            MyEnumTwinSliceEncoderExtensions.EncodeMyEnumTwin,
+            MyEnumHelper.read);
+
+        Assert.That(decodedValue, Is.EqualTo(MyEnum.Enum1));
+    }
+
+    private static TSliceEnum IceToSlice<TIceEnum, TSliceEnum>(
+        TIceEnum value,
+        Action<OutputStream, TIceEnum> encodeAction,
+        DecodeFunc<TSliceEnum> decodeFunc)
+    {
+        using Communicator communicator = Util.initialize();
+        var outputStream = new OutputStream(communicator);
+        encodeAction(outputStream, value);
+        byte[] buffer = outputStream.finished();
+
+        var decoder = new SliceDecoder(buffer, SliceEncoding.Slice1);
+        return decodeFunc(ref decoder);
+    }
+
+    private static TIceEnum SliceToIce<TSliceEnum, TIceEnum>(
+        TSliceEnum value,
+        EncodeAction<TSliceEnum> encodeAction,
+        Func<InputStream, TIceEnum> decodeFunc)
+    {
+        var pipe = new Pipe();
+        var encoder = new SliceEncoder(pipe.Writer, SliceEncoding.Slice1);
+        encodeAction(ref encoder, value);
+        pipe.Writer.Complete();
+        pipe.Reader.TryRead(out ReadResult readResult);
+
+        using Communicator communicator = Util.initialize();
+        var inputStream = new InputStream(communicator, readResult.Buffer.ToArray());
+        pipe.Reader.Complete();
+
+        return decodeFunc(inputStream);
+    }
+}
---
+++ tests/Interop.Tests/Slice/EnumTests.ice
@@ -0,0 +1,15 @@
+// Copyright (c) ZeroC, Inc.
+
+module Interop
+{
+    module Tests
+    {
+        module Slice
+        {
+            enum MyEnum
+            {
+                Enum1 = 187
+            }
+        }
+    }
+}
